dad3b5edf8d63f0a27814aa020bab7444589bb37
Mehdi Chekori <Mehdichekori@gmail.com>
2016-12-28 16:13:30 -0500

    html page starting to look like a chat

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ connections = [];
 server.listen(process.env.Port || 3000);
 console.log('server running');
 
-app.get('/',function(req,res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static('public'));
+// app.get('/',function(req,res){
+//   res.sendFile(__dirname + '/public/index.html');
+//   //res.sendFile(__dirname + '/Style/style.css');
+// });
+
+io.sockets.on('connection', function(socket){
+  //on connect
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+
+  //on disconnect
+  connections.splice(connections.indexOf(socket), 1);
+  console.log('Disconected: %s sockets connected', connections.length);
+})
