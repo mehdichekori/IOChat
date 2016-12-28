479848e9472313f6ecb7689299344d5f274ee335
mehdichekori <mehdichekori@gmail.com>
2016-12-28 18:19:26 -0500

    added functionalities
    added users, username before the message, pressing enter to send the message

--- public/Scripts/logic.js
+++ public/Scripts/logic.js
@@ -1,17 +1,52 @@
 $(function(){
-    var socket = io.connect();
-    var $messageForm = $('#messageForm');
-    var $message = $('#message');
-    var $chat = $('#chat');
+  var socket = io.connect();
+  var $messageForm = $('#messageForm');
+  var $message = $('#message');
+  var $chat = $('#chat');
+  var $messageArea = $('#messageArea');
+  var $userFormArea = $('#userFormArea');
+  var $userForm = $('#userForm');
+  var $users = $('#users');
+  var $username = $('#username');
 
-    $messageForm.submit(function(e){
-      e.preventDefault();
-      socket.emit('send message',$message.val());
-      $message.val('');
+
+  $messageForm.submit(function(e){
+    e.preventDefault();
+    socket.emit('send message',$message.val());
+    $message.val('');
+  });
+
+  socket.on('new message',function(data){
+    $chat.append('<div class="well"><strong>'+data.user+':</strong> '+data.msg+'</div>')
+  });
+
+  $userForm.submit(function(e){
+    e.preventDefault();
+    socket.emit('new user',$username.val(), function(data){
+      if(data){
+        $userFormArea.hide();
+        $messageArea.show();
+      }
     });
+    $username.val('');
+  });
 
-    socket.on('new message',function(data){
-      $chat.append('<div class="well">'+data.msg+'</div>')
-    });
+  socket.on('get users', function(data){
+    var html = '';
+    for(i = 0;i<data.length;i++){
+      html += '<li class="list-group-item">'+data[i]+'</li>'
+    }
+    $users.html(html);
+  });
 
+  $message.on('keypress', function (e){
+    if (e.keyCode == 13){
+      submitMessage();
+    }
+  });
+
+  function submitMessage(){
+    socket.emit('send message',$message.val());
+    $message.val('');
+  }
 });
