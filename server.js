2084fde5ee07491d2f523623cfbb2a791b7fe675
Mehdi Chekori <Mehdichekori@gmail.com>
2016-12-28 15:24:50 -0500

    added requirements

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-server file
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+users = [];
+connections = [];
