4b2cb29da8cb2af8f002e5e16b11503b773a2199
Mehdi chekori <mehdichekori@gmail.com>
2017-01-06 19:08:05 -0500

    Update and rename logic.js to client.js
    renamed and changed time for typing

--- public/Scripts/client.js
+++ public/Scripts/client.js
@@ -89,12 +89,12 @@ $(function(){
     typing=true;
     lastTyped = new Date().getTime();
     socket.emit('user is typing');
-    var timeout=setTimeout(checkIfHasStoppedTyping,3000);
+    var timeout=setTimeout(checkIfHasStoppedTyping,1000);
 
   }
   function checkIfHasStoppedTyping(typing){
     var currentTime = new Date().getTime();
-    if (currentTime - lastTyped>3000 && !typing){ //checks if the lasttyped time was more than 10 secs ago
+    if (currentTime - lastTyped>1000 && !typing){ //checks if the lasttyped time was more than 10 secs ago
       socket.emit('user has stopped typing');
           typing = false;
     }
